4909b2245870ab786d090384233bd103c925f16b
David McLaughlin <davidmclaugh@gmail.com>
2017-04-06 19:27:45 -0700

    clearCanvas function now resets colors and doesn't interrupt fade interval

--- js/presets.js
+++ js/presets.js
@@ -1,4 +1,5 @@
 var clearCanvas = function(){
+  //Slider Settings
   $("#gravity").val(50);
   $("#drag").val(0);
   $("#forceX").val(10);
@@ -8,10 +9,22 @@ var clearCanvas = function(){
   $(".border-toggle").prop("checked", true);
   $(".disable-gravity").prop("checked", true)
   
+  //Checkbox settings
   borderOn = true;
   noGravity = false;
   balls = [];
   centersOfGravity = [];
 
+  //Color schema
+  $("#background-color-picker").spectrum("set", "rgb(214,214,214)");
+  $("ball-max-color").spectrum("set", "#ff0000");
+  $("ball-min-color").spectrum("set", "#0000ff");
+
+  maxColor = "#ff0000";
+  minColor = "#0000ff";
+  backgroundColor = "rgba(214,214,214,0.1)";
+
+  //Fade interval
+  clearInterval(fadeLoop);
   fadeLoop = setInterval(fadeOut, 5);
 }
