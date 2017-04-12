a37e8e783ca74d43b69ba6e96527a44d83addcb5
David McLaughlin <davidmclaugh@gmail.com>
2017-04-11 18:37:44 -0700

    Add ui and basic settings for circle obstacles

--- js/obstacleCircle.js
+++ js/obstacleCircle.js
@@ -2,6 +2,7 @@ var ObstacleCircle = function(x, y, size){
   this.x = x;
   this.y = y;
   this.size = size;
+  this.color = "#000000"
 }
 
 ObstacleCircle.prototype.draw = function(){
@@ -10,5 +11,20 @@ ObstacleCircle.prototype.draw = function(){
   gc.ctx.fillStyle = this.color;
   gc.ctx.fill();
   gc.ctx.closePath();
+}
+
+
+var UnplacedObstacleCircle = function(x,y,size){
+  this.x = x;
+  this.y = y;
+  this.size = size;
+}
+
+UnplacedObstacleCircle.prototype.draw = function(){
+  ui.ctx.beginPath();
+  ui.ctx.arc(this.x, this.y, 50*Math.abs(this.size), 0, 2 * Math.PI, false);
+  ui.ctx.fillStyle = 'black';
+  ui.ctx.fill();
+  ui.ctx.closePath();
 }
 
